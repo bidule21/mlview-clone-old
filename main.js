f62637d04805689cbddc575c84f6dba5dcb85c74
Martin Vonheim Larsen <martin@melvon.com>
2014-05-20 17:22:08 +0200

    Added range selection mechanism

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ if (window.devicePixelRatio) {
     meta.content = 'width=device-width, user-scalable=no, initial-scale=' + (1/window.devicePixelRatio);
 }
 
+// find active range
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var rangeName = getParameterByName('range');
+
 // setup views
 var rangeView = new LiveShot.MegalinkRangeView();
 document.body.appendChild(rangeView.el);
@@ -14,7 +24,7 @@ document.body.appendChild(rangeView.el);
 var watcher = new MegalinkWatcher();
 watcher.on('update', function (ranges) {
     if (ranges.length > 0) {
-        rangeView.setRange(ranges[0], true);
+        setRanges(ranges);
         hideSpinner();
     } else {
         document.body.innerHTML = 'Waiting for data...';
@@ -31,6 +41,7 @@ watcher.start();
 updateSize();
 window.onresize = updateSize;
 
+
 function updateSize() {
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
@@ -40,4 +51,19 @@ function updateSize() {
 
     rangeView.updateSize();
     rangeView.draw();
+}
+
+function setRanges(ranges) {
+    var range = ranges[0]; // select the first range by default
+
+    if (rangeName) {
+        for (var idx in ranges) {
+            if (ranges[idx].name == rangeName) {
+                range = ranges[idx];
+                break;
+            }
+        }
+    }
+
+    rangeView.setRange(range, true);
 }
